e20b13defab447613aada5ca7588dbef796301b9
Airan Omar <alu0101071039@cc1412.etsii.ull.es>
2018-05-02 10:30:14 +0100

    Caso de uso 2, Error return assertion.

--- practicaTDD.js
+++ practicaTDD.js
@@ -5,4 +5,7 @@ function Raices(a, b, c){
   return [[5, 0], [2, 0]];
 }
 
+// Caso de uso 1.
 assert.deepEqual(Raices(1, -5, 0), [[5, 0], [2, 0]]);
+// Caso de uso 2.
+assert.deepEqual(Raices(2, 5, 0), [[0, 0], [2.5, 0]]);
