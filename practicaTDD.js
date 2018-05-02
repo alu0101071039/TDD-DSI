d203394ddcef338dc04e78befeb9f1d77df1151d
Airan Omar <alu0101071039@cc1412.etsii.ull.es>
2018-05-02 10:35:43 +0100

    Caso de uso 2, Refactorizado.

--- practicaTDD.js
+++ practicaTDD.js
@@ -3,10 +3,12 @@ var assert = require('assert');
 // a*x^2 + b*x + c
 function Raices(a, b, c){
 
-  var R1 = (-b + Math.sqrt(b*b - 4 * a * c)) / (2 * a);
-  var R2 = (-b - Math.sqrt(b*b - 4 * a * c)) / (2 * a);
+  var Discriminante = Math.sqrt(b*b - 4 * a * c);
 
-  return [[R1, 0], [R2, 0]];
+  var Raiz1 = (-b + Discriminante) / (2 * a);
+  var Raiz2 = (-b - Discriminante) / (2 * a);
+
+  return [[Raiz1, 0], [Raiz2, 0]];
 }
 
 // Caso de uso 1.
