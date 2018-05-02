d97735302aa4d86b3b439790270eeaaa6613c22e
Airan Omar <alu0101071039@cc1412.etsii.ull.es>
2018-05-02 10:34:04 +0100

    Caso de uso 2, Corregido.

--- practicaTDD.js
+++ practicaTDD.js
@@ -2,10 +2,14 @@ var assert = require('assert');
 
 // a*x^2 + b*x + c
 function Raices(a, b, c){
-  return [[5, 0], [2, 0]];
+
+  var R1 = (-b + Math.sqrt(b*b - 4 * a * c)) / (2 * a);
+  var R2 = (-b - Math.sqrt(b*b - 4 * a * c)) / (2 * a);
+
+  return [[R1, 0], [R2, 0]];
 }
 
 // Caso de uso 1.
-assert.deepEqual(Raices(1, -5, 0), [[5, 0], [2, 0]]);
+assert.deepEqual(Raices(1, -5, 0), [[5, 0], [0, 0]]);
 // Caso de uso 2.
-assert.deepEqual(Raices(2, 5, 0), [[0, 0], [2.5, 0]]);
+assert.deepEqual(Raices(2, 5, 0), [[0, 0], [-2.5, 0]]);
