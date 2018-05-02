17eb0619ec7cbde6798ee35c8ae78ac46b055b77
Airan Omar <alu0101071039@cc1412.etsii.ull.es>
2018-05-02 10:52:34 +0100

    Caso de uso 4, Refactorizado.

--- practicaTDD.js
+++ practicaTDD.js
@@ -4,19 +4,22 @@ var assert = require('assert');
 function Raices(a, b, c){
 
   var Discriminante = b*b - 4 * a * c;
+  var RaizDiscriminante;
+  var RaizReal1, RaizReal2, RaizImaginaria1, RaizImaginaria2;
+  var DobleA = 2 * a;
 
   if(Discriminante > 0){
     var RaizDiscriminante = Math.sqrt(Discriminante);
-    var RaizReal1 = (-b + RaizDiscriminante) / (2 * a);
-    var RaizReal2 = (-b - RaizDiscriminante) / (2 * a);
+    var RaizReal1 = (-b + RaizDiscriminante) / DobleA;
+    var RaizReal2 = (-b - RaizDiscriminante) / DobleA;
     var RaizImaginaria1 = 0;
     var RaizImaginaria2 = 0;
   }else{
     var RaizDiscriminante = Math.sqrt(-Discriminante);
     var RaizReal1 = 0;
     var RaizReal2 = 0;
-    var RaizImaginaria1 = (-b + RaizDiscriminante) / (2 * a);
-    var RaizImaginaria2 = (-b - RaizDiscriminante) / (2 * a);
+    var RaizImaginaria1 = (-b + RaizDiscriminante) / DobleA;
+    var RaizImaginaria2 = (-b - RaizDiscriminante) / DobleA;
   }
 
   return [[RaizReal1, RaizImaginaria1], [RaizReal2, RaizImaginaria2]];
