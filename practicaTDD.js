027e05de6b7f96738ca7df4fc963852aa70703ab
Airan Omar <alu0101071039@cc1412.etsii.ull.es>
2018-05-02 10:26:56 +0100

    Caso de uso 1, Corrigiendo el error, no hace falta refactorizar.

--- practicaTDD.js
+++ practicaTDD.js
@@ -1,3 +1,8 @@
 var assert = require('assert');
 
-assert.deepEqual(Raices(1, -5, 0),[[5, 0],[2, 0]]);
+// a*x^2 + b*x + c
+function Raices(a, b, c){
+  return [[5, 0], [2, 0]];
+}
+
+assert.deepEqual(Raices(1, -5, 0), [[5, 0], [2, 0]]);
